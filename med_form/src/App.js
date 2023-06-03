7b75f443ea3087992902fafbd79b5ef09ad03a6c
Anhelina_Lebedieva <angelina.lebedieva@gmail.com>
2023-06-03 23:22:53 +0300

    Name, BD, Sex

--- med_form/src/App.js
+++ med_form/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import DateChanger from "./dateChange/dateChange";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="form">
+      <label>Name</label>
+      <input
+        {...register("name", {
+          required: true,
+          pattern: /^([a-zA-Z]+\s)*[a-zA-Z]+$/,
+        })}
+      />
+      {errors.name && <p className="form__error">Please enter a valid name</p>}
+      <label>Birthday Date</label>
+      <DateChanger reg={register("date")} />
+      <label>Sex</label>
+      <select {...register("gender", { required: true })}>
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+      </select>
+      <input type="submit" />
+    </form>
   );
 }
 
---
+++ med_form/src/dateChange/dateChange.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function DateChanger(props) {
+  const [date, setDate] = useState("");
+  const onDateChange = (e) => {
+    console.log("onDateChange: ", e.target.value);
+    setDate(e.target.value);
+  };
+
+  return (
+    <input
+      {...props.reg}
+      onChange={onDateChange}
+      type="date"
+      name="date"
+      required
+      placeholder="dd-mm-yyyy"
+      value={date}
+      min="1900-01-01"
+      max="2030-12-31"
+    />
+  );
+}
+
+export default DateChanger;
