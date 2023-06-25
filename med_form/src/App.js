7b74d4bed76ba87921158f73663357471553b7a4
Anhelina_Lebedieva <angelina.lebedieva@gmail.com>
2023-06-25 19:58:25 +0300

    Gender,City, Doc, Spec

--- med_form/src/App.js
+++ med_form/src/App.js
@@ -14,10 +14,16 @@ function App() {
   const [cities, setCities] = useState([]);
   const [specialities, setSpecialties] = useState([]);
   const [doctors, setDoctors] = useState([]);
+
   const [doctorsF, setDoctorsF] = useState([]);
   const [specF, setSpecF] = useState([]);
   const [cityF, setCityF] = useState([]);
+
   const [date, setDate] = useState("");
+  const [gender, setGender] = useState("");
+  const [city, setCity] = useState("");
+  // const [specialy, setSpecialty] = useState("");
+  // const [doctor, setDoctor] = useState("");
 
   const fetchCitiesData = () => {
     fetch("https://run.mocky.io/v3/9fcb58ca-d3dd-424b-873b-dd3c76f000f4")
@@ -47,37 +53,30 @@ function App() {
     fetchCitiesData();
   }, []);
 
-  function dataCheck(filtered, basic) {
-    let chosen = [];
-    if (filtered.length === 0) {
-      chosen = basic;
-    } else chosen = filtered;
-    return chosen;
-  }
-
   const handleCityChange = (selected) => {
     let selectedCityName = selected.target.value;
     let selectedCity = cities.find((e) => e.name === selectedCityName);
-    let filteredDocs = [...doctors].filter(
-      (doctor) => doctor.cityId === selectedCity.id
-    );
-    setDoctorsF(filteredDocs);
+    let filteredDocs = [];
 
-    let specFiltered = [];
-    for (let i = 0; i < filteredDocs.length; i++) {
-      for (let j = 0; j < specialities.length; j++) {
-        if (specialities[j].id === filteredDocs[i].specialityId) {
-          specFiltered.push(specialities[j]);
-        }
-      }
+    setCity(selectedCity);
+
+    if (gender !== "") {
+      filteredDocs = genderFilter(gender).filter(
+        (doctor) => doctor.cityId === selectedCity.id
+      );
+    } else {
+      filteredDocs = [...doctors].filter(
+        (doctor) => doctor.cityId === selectedCity.id
+      );
     }
-    setSpecF(specFiltered);
+
+    setDoctorsF(filteredDocs);
+    specFilter(filteredDocs);
   };
 
   function genderFilter(gender) {
     let specFilteredIndex = [];
     let specFiltered = [...specialities];
-    let specChosen = dataCheck(specF, specialities);
 
     specFilteredIndex = specFiltered.filter(
       (spec) =>
@@ -87,53 +86,106 @@ function App() {
     );
 
     for (let j = 0; j < specFilteredIndex.length; j++) {
-      for (let i = 0; i < specChosen.length; i++) {
-        if (specChosen[i] === specFilteredIndex[j]) {
+      for (let i = 0; i < specialities.length; i++) {
+        if (specialities[i] === specFilteredIndex[j]) {
           delete specFiltered[i];
         }
       }
     }
 
     setSpecF(specFiltered.filter(Boolean));
+
+    docFilter(specFiltered.filter(Boolean));
+    cityFilter(docFilter(specFiltered.filter(Boolean)));
+    return docFilter(specFiltered.filter(Boolean));
   }
 
   const handleGenderChange = (selected) => {
     if (selected.target.value === "Male") {
+      setGender("Female");
       genderFilter("Female");
     }
     if (selected.target.value === "Female") {
+      setGender("Male");
       genderFilter("Male");
     }
   };
 
-  const handleDoctorChange = (selected) => {
-    let selectedDocName = selected.target.value;
-    let selectedDoc = doctors.find((e) => e.name === selectedDocName);
+  function cityFilter(selectedDocs) {
+    let filteredCity = [];
 
-    let filteredCity = [...cities].filter(
-      (city) => city.id === selectedDoc.cityId
-    );
-    setCityF(filteredCity);
+    for (let i = 0; i < selectedDocs.length; i++) {
+      for (let j = 0; j < cities.length; j++) {
+        if (cities[j].id === selectedDocs[i].cityId) {
+          filteredCity.push(cities[j]);
+        }
+      }
+    }
 
+    let uniqueFilteredCity = [...new Set(filteredCity)];
+
+    setCityF(uniqueFilteredCity);
+    return uniqueFilteredCity;
+  }
+
+  function specFilter(selectedDocs) {
     let specFiltered = [];
-    for (let i = 0; i < filteredCity.length; i++) {
+    for (let i = 0; i < selectedDocs.length; i++) {
       for (let j = 0; j < specialities.length; j++) {
-        if (specialities[j].id === selectedDoc.specialityId) {
+        if (specialities[j].id === selectedDocs[i].specialityId) {
           specFiltered.push(specialities[j]);
         }
       }
     }
-    setSpecF(specFiltered);
+
+    let uniqueSpec = [...new Set(specFiltered)];
+    setSpecF(uniqueSpec);
+    return uniqueSpec;
+  }
+
+  function docFilter(selectedSpec) {
+    let docFiltered = [];
+    for (let i = 0; i < doctors.length; i++) {
+      for (let j = 0; j < selectedSpec.length; j++) {
+        if (selectedSpec[j].id === doctors[i].specialityId) {
+          docFiltered.push(doctors[i]);
+        }
+      }
+    }
+
+    setDoctorsF(docFiltered);
+    return docFiltered;
+  }
+
+  const handleDoctorChange = (selected) => {
+    let selectedDocName = selected.target.value;
+    let selectedDoc = doctors.find((e) => e.name === selectedDocName);
+    let selectedDocs = [];
+
+    selectedDocs.push(selectedDoc);
+
+    cityFilter(selectedDocs);
+    specFilter(selectedDocs);
   };
 
   const handleSpecChange = (selected) => {
     let selectedSpecName = selected.target.value;
     let selectedSpec = specialities.find((e) => e.name === selectedSpecName);
-    let filteredDocs = [...doctors].filter(
-      (doctor) => doctor.specialityId === selectedSpec.id
-    );
+    let filteredDocs = [];
 
+    if (city !== "") {
+      filteredDocs = [...doctors].filter(
+        (doctor) =>
+          doctor.specialityId === selectedSpec.id && city.id === doctor.cityId
+      );
+    } else {
+      filteredDocs = [...doctors].filter(
+        (doctor) => doctor.specialityId === selectedSpec.id
+      );
+    }
     setDoctorsF(filteredDocs);
+
+    cityFilter(filteredDocs);
   };
 
   const handleDateChange = (selected) => {
@@ -148,12 +200,17 @@ function App() {
     let filteredDocs = [];
     if (chosenDate > eighteenYearsAgo) {
       filteredDocs = doctors.filter((doctor) => doctor.isPediatrician === true);
-    } else
+    } else if (chosenDate < eighteenYearsAgo) {
       filteredDocs = doctors.filter(
         (doctor) => doctor.isPediatrician === false
       );
+    }
+
+    console.log(filteredDocs);
 
     setDoctorsF(filteredDocs);
+    cityFilter(filteredDocs);
+    specFilter(filteredDocs);
   };
 
   return (
